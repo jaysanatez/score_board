70a7a9fc5cd56b095b941dc2a452b00ba9ae4281
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2015-07-05 12:57:20 -0500

    Women sports can be created

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -3,7 +3,7 @@ class Sport < ActiveRecord::Base
 	has_many :schools, :through => :teams
 
 	validates :name, presence: true 
-	validates :male, presence: true 
+	validates :male, :inclusion => {:in => [true, false]} 
 
 	def to_string
 		return "#{name} - #{if male then "Men's" else "Women's" end}" 
